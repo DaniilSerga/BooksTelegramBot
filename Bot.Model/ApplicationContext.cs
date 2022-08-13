ce93648e96083a61b1dc44c7944d9c05ddd4880c
Daniel Serga <daniil.serga@gmail.com>
2022-08-13 11:33:57 +0300

    first changes

---
+++ Bot.Model/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bot.Model.Models;
+
+namespace Bot.Model
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Bot.Model/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bot.Model.Models
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
